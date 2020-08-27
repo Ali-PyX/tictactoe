d0acfbe6102e302d9f1d01dcd591858f26e29f43
ali pyX <ali.pyix@gmail.com>
2020-08-27 11:47:54 +0430

    def base functions

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-#tic tac toe Game
+# tic tac toe Game
+
+# init board
+board = []
+
+# show current board
+def show_board():
+    pass
+
+
+# select move
+def select_action():
+    pass
+
+# move as player select and mark
+def move():
+    pass
+
+
+# flip player mark between X and O
+def flip_player():
+    pass
+
+def check_game_is_over():
+
+    def check_rows():
+        pass
+
+
+    def check_coulmns():
+        pass
+
+
+    def check_diagonals():
+        pass
 
 
 # main function that run at the end
